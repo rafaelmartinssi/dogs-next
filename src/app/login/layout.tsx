c98d19e8b9b4e228e59e58a2de8b8f3d53a96c6b
Rafael Martins Galdino <rafael_martins_182@hotmail.com>
2024-08-24 17:05:42 -0300

    Create Layout Login

---
+++ src/app/login/layout.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "@/app/login/login.module.css";
+
+export default async function LoginLayout ({children}: {children: React.ReactNode}) {
+  return (
+    <div className={styles.login}>
+      <div className={styles.forms}>{children}</div>
+    </div>
+  )
+}
